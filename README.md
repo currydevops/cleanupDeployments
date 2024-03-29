79480e3000cd7a02fa3568b1e406f5e0b126bf84
Roshan Patel <patelroshan21@yahoo.com>
2024-03-29 01:53:32 -0700

    refactor files, update README, gitignore

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 ###
+## Assumptions
+1. Docker is installed, localstack is already installed and aws s3 service is running
+1. You have a virtual env already setup for python(optional)
+
+
+## Setup
+Check if s3 service is running in localstack, the output should be `running`
+```
+localstack status services -f json | jq '.s3'
+
+docker ps | grep -i "localstack" ##docker container named localstack should be running/healthy  
+```
+
+We need to create a aws profile `localstack`, you can set the `AWS Access Key ID=test`,  `AWS Secret Access Key=test`, `region=us-east-1`
+```
+aws configure --profile localstack
+```
+
+Create a s3 bucket called `deployments`
+```
+AWS_PROFILE=localstack aws s3 mb s3://deployments --endpoint-url http://localhost:4566ck 
+```
+
+
+## Create fake data in the localstack `deployments` s3 buckets
+On your terminal, cd into the git repository, run the following command
+
+`pip install -r requirements.txt`
+
+
+`python -m createFakeData`
+
+```
+The output should something like this 
+Uploaded file 'deployhash112/index.html' successfully.
+Uploaded file 'deployhash112/css/font.css' successfully.
+Uploaded file 'deployhash112/images/hey.png' successfully.
+Uploaded file 'dsfsfsl9074/root.html' successfully.
+Uploaded file 'dsfsfsl9074/styles/font.css' successfully.
+Uploaded file 'dsfsfsl9074/img/hey.png' successfully.
+Uploaded file 'delkjlkploy3/base.html' successfully.
+Uploaded file 'delkjlkploy3/fonts/font.css' successfully.
+Uploaded file 'delkjlkploy3/png/hey.png' successfully.
+Uploaded file 'klljkjkl123/index.html' successfully.
+Uploaded file 'klljkjkl123/css/font.css' successfully.
+Uploaded file 'klljkjkl123/images/hey.png' successfully.
+Uploaded file 'dsfff1234321/root.html' successfully.
+Uploaded file 'dsfff1234321/styles/font.css' successfully.
+Uploaded file 'dsfff1234321/img/hey.png' successfully.
+```
+
+Use the following command to check data in s3
+```
+AWS_PROFILE=localstack aws s3 ls s3://deployments --endpoint-url http://localhost:4566 --recursive
+```
+
+
+Next run the cleanDeployments.py script, `keep-x-deployments` is the # of deployment to keep, default is `1`
+
+`python -m cleanupDeploys --keep-x-deployments 2`
+```
+2024-03-29 01:44:15,265 - INFO - There are total of 5 deployments we are keeping 2 deployments
+2024-03-29 01:44:15,265 - INFO - These deployments will be deleted ['delkjlkploy3', 'dsfsfsl9074', 'deployhash112']
+2024-03-29 01:44:15,319 - INFO - Deleted deployment: delkjlkploy3
+2024-03-29 01:44:15,343 - INFO - Deleted deployment: dsfsfsl9074
+2024-03-29 01:44:15,365 - INFO - Deleted deployment: deployhash112
+```
--- cleanupDeploys.py
+++ cleanupDeploys.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
+import argparse
 import boto3
 from collections import defaultdict
 import logging
 
 class DeploymentManager:
-    def __init__(self, bucket_name):
+    def __init__(self, bucket_name, profile_name):
         """
         Initialize the DeploymentManager object with the given S3 bucket name.
 
@@ -12,7 +15,7 @@ class DeploymentManager:
         """
         self.bucket_name = bucket_name
         # Initialize a boto3 session with localstack profile
-        self.session = boto3.Session(profile_name='localstack')
+        self.session = boto3.Session(profile_name=profile_name)
         # Create an S3 client
         self.s3_client = self.session.client('s3', endpoint_url='http://localhost:4566')
 
@@ -65,7 +68,6 @@ class DeploymentManager:
         """
         upload_dates = {}
         for deployment, objects in deployments.items():
-            # Get the last modified date for each deployment
             last_modified_values = []
             # Iterate over each object in the objects list
             for obj in objects:
@@ -93,7 +95,6 @@ class DeploymentManager:
         ordered_deployments = self.group_deployments(objects=deployments_list)
         # Sort deployments by their upload dates
         sorted_deployments = self.sort_deployments_by_upload_date(ordered_deployments)
-        # Get deployments to be deleted
         logging.info(f'There are total of {len(sorted_deployments)} deployments we are keeping {keep_x_deployment} deployments')
         x_deployment = sorted_deployments[keep_x_deployment:]
         logging.info(f'These deployments will be deleted {x_deployment}')
@@ -108,11 +109,40 @@ class DeploymentManager:
                     s3_resource.Object(bucket_name, obj['Key']).delete()
             logging.info(f"Deleted deployment: {deployment}")
 
+    def delete_deployments_xdays_ydeploys(self, keep_x_days, y_deploys):
+        deployments_list = self.get_list_of_deployments()
+        # Group deployments by their names
+        sorted_deployments = sorted(deployments_list, key=lambda x: x['LastModified'], reverse=True)
+
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Cleanup old deployments in S3 bucket')
+    parser.add_argument('--keep-x-deployments', type=int, default=1, help='Number of deployments to keep (default: 1)')
+    args = parser.parse_args()
+
     # Define the S3 bucket name
     bucket_name = "deployments"
     # Initialize a DeploymentManager object
-    manager = DeploymentManager(bucket_name)
+    manager = DeploymentManager(bucket_name, profile_name='localstack')
     # Delete old deployments, keeping only the most recent ones
-    manager.delete_deployments(keep_x_deployment=2)
+    manager.delete_deployments(args.keep_x_deployments)
+
+'''
+List Objects in Bucket: get_list_of_deployments()
+The script starts by listing all objects in the S3 bucket s3-bucket-name.
+For each object, it retrieves information including the object key and its LastModified timestamp.
+
+Group Objects by Deployment: group_deployments(objects=deployments_list)
+It then groups the objects by their parent deployment folders (deploy1, deploy2, etc.).
+Each deployment folder becomes a key in the dictionary, and the corresponding value is a list of objects belonging to that deployment.
+
+Sort Deployments by Creation/last-modified Date: sort_deployments_by_upload_date(ordered_deployments)
+The script sorts the deployments based on the creation date of their objects.
+For example, deploy1 has objects with modification dates on 2023-03-10, deploy2 on 2023-03-09, and so on.
+
+Delete Old Deployments:
+Since we want to keep only the two most recent deployments, the script selects the top two deployments from the sorted list (ex deploy1 and deploy2).
+It then deletes the objects belonging to the older deployments dirs&files (deploy3, deploy4, and deploy5) using the get_list_of_deployments() dictionary output
+
+'''
